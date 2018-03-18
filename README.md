1016006ec3accdce799b4bba02fc0b7b4b43efc0
Naterra <natalinyweb@hotmail.com>
2018-03-17 23:52:22 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## App
+
+#### Heroku
+<pre><code>git init
+heroku login
+heroku git:remote -a travel-mern</code></pre>
+
+Initial commit
+<pre><code>git add .</pre></code>
---
+++ src/client/Routes.js
@@ -0,0 +1,41 @@
+//http://react-ssr-api.herokuapp.com/
+
+import React from 'react';
+import App from './App';
+import HomePage from './pages/HomePage';
+import UsersListPage from './pages/UsersListPage';
+import NotFoundPage from './pages/NotFoundPage';
+import AdminsListPage from './pages/AdminsListPage';
+
+export default [
+	{
+		...App,
+		routes: [
+			{
+				...HomePage,
+				path: '/',
+				exact: true
+			},
+			{
+				...AdminsListPage,
+				path: '/admins'
+			},
+			{
+				...UsersListPage,
+				path: '/users'
+			},
+			{
+				...NotFoundPage
+			}
+		]
+	}
+];
+
+// export default ()=>{
+//     return (
+//         <div>
+//             <Route exact path="/" component={Home} />
+//             <Route       path="/users" component={UsersList} />
+//         </div>
+//     );
+// };
