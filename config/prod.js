a5ab89b45e414344a17a96b900885eab0027fd6e
Naterra <natalinyweb@hotmail.com>
2018-08-26 20:16:43 -0400

    Remove the now ignored directory node_modules

--- config/prod.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    "apiPrefix": "http://localhost:3000/",
-    "serverPort": "3000",
-    "cookieKey":  process.env.cookieKey,
-    "mongo_URL":  process.env.mongo_URL,
-    "googleAuth":{}
-};
